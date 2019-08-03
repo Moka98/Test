2335ad43e7ba56ecce09249c14f44d515a2a8aa5
Moka98 <mplmok001@myuct.ac.za>
2019-08-03 17:52:10 +0100

    First version of Prac1 code

---
+++ Prac1.py
@@ -0,0 +1,32 @@
+"""
+Names: Mopeli Moka
+Student Number: MPLMOK001
+Prac: Prac 1
+Date: 
+"""
+
+#Import Libraries
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.OUT)
+ 
+def main():
+	GPIO.output(11,GPIO.HIGH)
+	print("ON")
+	time.sleep(2)
+	GPIO.output(11,GPIO.LOW)
+	print("OFF")
+	time.sleep(2)
+
+if __name__ == "__main__":
+	try:
+	   while True:
+	        main()
+	except KeyboardInterrupt:
+	      print("Exeting gracefully")
+	      GPIO.cleanup()
+	except Exception as e:
+	     GPIO.cleanup()
+	     print("Some other error occurred")
+	     print(e.message)
